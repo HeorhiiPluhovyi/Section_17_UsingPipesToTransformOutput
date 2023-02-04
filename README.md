74e72830d33d66728ef763fe051d818e81b49547
Heorhii Pluhovyi <heorhiipluhovui@gmail.com>
2023-02-04 07:06:22 +0200

    save

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# [DEMO LINK](https://courageous-bavarois-196443.netlify.app/)
+
 # Section17UsingPipesToTransformOutput
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.5.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
+import { FilteredPipe } from './filtered.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe,
+    FilteredPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
